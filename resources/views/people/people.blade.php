89eb9bb6fab04cd7345746a0e8f0d4fd295ad8b5
Thiago Mendes <th.thiago.mendes@gmail.com>
2019-02-04 21:47:40 -0200

    fix show all button

--- resources/views/people/people.blade.php
+++ resources/views/people/people.blade.php
@@ -48,7 +48,7 @@
                     </div>
                     <tc-person v-bind:people="{{$retorno['people']}}"></tc-person>
                 </div>
-                @if (count($retorno['people']) > 10)
+                @if ($retorno['number'] > 10)
                     <div class="x_content">
                         <a href="/pessoas/todas"><button type="button" class="btn btn-success">Mostrar todos</button></a>
                     </div>                                                       
